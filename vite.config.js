e0681e4f348aa334cc8780a692142c2510843722
Mkznd <yzundai1@gmail.com>
2023-08-21 16:18:50 +0200

    Added an action to publish

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,15 @@
 import { defineConfig } from "vite";
 import viteReact from "@vitejs/plugin-react";
 
-export default defineConfig({
-  plugins: [viteReact()],
+export default defineConfig(({ command }) => {
+  const config = {
+    plugins: [viteReact()],
+    base: "/",
+  };
+
+  if (command !== "serve") {
+    config.base = "/shoppable_demo/";
+  }
+
+  return config;
 });
